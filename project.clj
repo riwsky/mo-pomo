dc03ca51694d9dcb7bd170b84aeb152f33a2ba20
William Cybriwsky <william.cybriwsky@gmail.com>
2016-01-27 00:26:11 -0500

    Add lein-cljsbuild and a "prod" config

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.170"]
                  [sablono "0.3.6"]]
-  :plugins [[lein-figwheel "0.5.0-1"]]
+  :plugins [[lein-figwheel "0.5.0-1"]
+            [lein-cljsbuild "1.1.2"]]
   :clean-targets [:target-path "out" "resources/public/cljs"]
   :cljsbuild {
               :builds [{:id "dev"
@@ -12,7 +13,14 @@
                                    :asset-path "cljs/out"
                                    :output-to "resources/public/cljs/main.js"
                                    :output-dir "resources/public/cljs/out"}
-                        }]
+                        }
+                       {:id "prod"
+                        :source-paths ["src"]
+                        :compiler {:asset-path "cljs/out"
+                                   :output-to "resources/public/cljs/main.js"
+                                   :optimizations :advanced}
+                        }
+                       ]
               }
   :figwheel {
              :css-dirs ["resources/public/css"]
