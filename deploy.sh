07183ed5135de36443b86737a3a81491b40716ee
William Cybriwsky <william.cybriwsky@gmail.com>
2016-02-01 23:54:10 -0500

    Automate the gh-pages rebuild and update

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Merge master to gh-pages, rebuild the prod cljs, and push.
+
+set -e
+
+git checkout gh-pages
+git merge master
+rm resources/public/cljs/main.js
+lein cljsbuild once prod
+git add -f resources/public/cljs/main.js
+git commit -m "Update gh-pages js"
+git push
